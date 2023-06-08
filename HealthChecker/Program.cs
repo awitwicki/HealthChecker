435f5226fd2534fcb4e4fe93d406497e53422ec5
Andrew Witwicki <vitvitskiy3@gmail.com>
2023-06-08 13:40:47 +0200

    Code

---
+++ HealthChecker/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+using Cron.NET;
+using Telegram.Bot;
+
+// Get params from env
+var url = Environment.GetEnvironmentVariable("URL") ??
+          throw new ArgumentNullException("url is null");
+
+var botToken = Environment.GetEnvironmentVariable("BOT_TOKEN") ??
+               throw new ArgumentNullException("BOT_TOKEN is null");
+
+var chatId = long.Parse(Environment.GetEnvironmentVariable("CHAT_ID") ??
+                        throw new ArgumentNullException("CHAT_ID is null"));
+
+var cronSchedule = Environment.GetEnvironmentVariable("CRON_SCHEDULE") ??
+                   throw new ArgumentNullException("CRON_SCHEDULE is null");
+
+Console.WriteLine("Starting HealthChecker");
+
+async Task<bool> CheckUrl(string url)
+{
+    var client = new HttpClient();
+    var response = await client.GetAsync(url);
+
+    return response.IsSuccessStatusCode;
+}
+
+async Task SendLog(string message)
+{
+    var bot = new TelegramBotClient(botToken);
+    await bot.SendTextMessageAsync(chatId, message);
+}
+
+var logAction = new Action(async () => 
+{
+    var dateStr = DateTimeOffset.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
+
+    try
+    {
+        var watch = Stopwatch.StartNew();
+        var isOk = await CheckUrl(url);
+        watch.Stop();
+        var elapsedMs = watch.ElapsedMilliseconds;
+
+        var statusStr = isOk ? "OK" : "NOT OK";
+        
+        if (!isOk)
+        {
+            await SendLog($"Health check: Not respond! ❌️️\n{url}");
+        }
+
+        Console.WriteLine($"{dateStr} (UTC) | Status: {statusStr} | Response time: {elapsedMs} ms");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"{dateStr} (UTC)");
+        Console.WriteLine(ex.Message);
+    }
+});
+
+var daemon = new CronDaemon();
+
+daemon.AddJob("* * * * *", logAction);
+
+daemon.Start();
+
+// Wait and sleep forever. Let the cron daemon run.
+await Task.Delay(Timeout.Infinite);
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # HealthChecker
-Simple docker service to check url availability and sends alert to telegram if it's unavailable
+Simple docker service to check url availability and sends alert to telegram if it's unavailable (response HTTP code is not 200x)
+
+
+## Configure
+
+Use next environment variables:
+
+* `URL={YOUR_URL}` - URL to check
+* `TELEGRAM_TOEN={YOUR_TOKEN}` - telegram bot token
+* `CRON_SCHEDULE={YOUR_TOKEN}` - 0 * * * * - every hour
+* `CHAT_ID={SPECIFIED_CHAT_ID}` - telegram chat id to send logs
+
+
+1. Create `.env` file and fill it with that variables.
+
+2. Run command `docker-compose up -d` this will start building and run service in docker.
+
